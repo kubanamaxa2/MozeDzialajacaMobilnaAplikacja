3072dd165a2c0792f1cc4ef74fa9070932a9cc7a
Dominos0321 <dsch792@gmail.com>
2022-11-04 14:25:28 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mozedzialajacamobilnaaplikacja/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.mozedzialajacamobilnaaplikacja
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.TextView
+import java.util.Stack
+import java.util.LinkedList
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var stos = Stack<String>()
+        stos.addAll(listOf("Jeden","Dwa","Trzy"))
+        var kolejka = LinkedList<String>()
+        kolejka.addAll(listOf("Mamuś","Tatuś","Babuś"))
+        var textwidze = findViewById<TextView>(R.id.textView2);
+        fun LIFO(){
+            textwidze.text = stos.peek()
+            findViewById<Button>(R.id.button).setOnClickListener {
+                stos.push(textwidze.text.toString())
+                textwidze.text = stos.peek()
+            }
+            findViewById<Button>(R.id.button2).setOnClickListener {
+                if(stos.isNotEmpty()){
+                    stos.pop()
+                    if(stos.isEmpty()){
+                        textwidze.text = " "
+                    }
+                    else {
+                        stos.peek()
+                    }
+                }
+                else{
+                    textwidze.text = " "
+                }
+
+            }
+        }
+        fun FIFO(){
+            textwidze.text = kolejka.first
+            findViewById<Button>(R.id.button2).setOnClickListener {
+            if(kolejka.isNotEmpty()){
+                kolejka.removeFirst()
+                if(kolejka.isEmpty()){
+                    textwidze.text = ""
+                }
+                else{
+                    textwidze.text = kolejka.first
+                }
+            }
+            else{
+                textwidze.text = ""
+            }
+            }
+            findViewById<Button>(R.id.button).setOnClickListener {
+                kolejka.addLast(findViewById<EditText>(R.id.editTextTextPersonName).toString())
+                textwidze.text = kolejka.first
+            }
+        }
+        findViewById<RadioButton>(R.id.radioButton).setOnClickListener{
+            
+        }
+    }
+}
